ad3a3dc6155b2c083818a1fc8b3f34708927634c
Jihad Benastey <jbenastey@gmail.com>
2020-04-27 00:13:39 +0700

    hasil prediksi

--- asset/js/apps.js
+++ asset/js/apps.js
@@ -13,8 +13,60 @@ $(document).ready(function () {
 			cache: false,
 			dataType: 'json',
 			success: function (response) {
-				console.log(response);
+				var nama = [];
+				var Ex = [];
+				var Ey = [];
+				var Exy = [];
+				var Ex2 = [];
+				var Ey2 = [];
+				var n = [];
+				var b = [];
+				var a = [];
+				var Y = [];
+				for (var i = 0; i < response.length ; i++) {
+					nama.push(response[i].nama_poli);
+					Ex.push(0);
+					Ey.push(0);
+					Exy.push(0);
+					Ex2.push(0);
+					Ey2.push(0);
+					n.push(0);
+					for (var j = 0; j < response[i].praproses.length; j++) {
+						var x = j+1;
+						var y = parseInt(response[i].praproses[j]);
+						var xy = x * y;
+						var x2 = x * x;
+						var y2 = y * y;
+
+						Ex[i] += x;
+						Ey[i] += y;
+						Exy[i] += xy;
+						Ex2[i] += x2;
+						Ey2[i] += y2;
+						n[i] = x;
+					}
+					b.push(cariB(Ex[i],Ey[i],Exy[i],Ex2[i],Ey2[i],n[i]));
+					a.push(cariA(Ex[i],Ey[i],b[i],n[i]));
+					Y.push(cariY(a[i],b[i],(n[i]+1)));
+				}
+				console.log(Y);
 			}
 		})
 	})
 });
+
+function cariB(Ex,Ey,Exy,Ex2,Ey2,n) {
+	var hasil = ((n*Exy) - (Ex*Ey)) / ((n*Ex2) - (Ex*Ex));
+	return hasil;
+}
+
+function cariA(Ex,Ey,b,n) {
+	var hasil = (Ey - (b*Ex)) / n;
+	return hasil
+}
+
+function cariY(a,b,n) {
+	var hasil = a + (b * n);
+	return hasil
+}
+
